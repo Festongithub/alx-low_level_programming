faf24f2a8e18b8b524e02f6a7bcd18cd2c58ccc6
Festongithub <fkemboik@gmail.com>
2022-09-12 03:17:26 -0700

    Soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by anew line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char ch = 'a';
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
- Alphabet
+Alphabet soup
