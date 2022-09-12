3863f4da071132a8b2309fd27ff1141d703f85eb
Festongithub <fkemboik@gmail.com>
2022-09-12 02:08:43 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this the main function
+ * description -prints all single digit number of base 10 starting from 0
+ */
+void  main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
