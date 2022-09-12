de5cade4ee916606f9b314a4e76ed018984bfa86
Festongithub <fkemboik@gmail.com>
2022-09-12 03:25:35 -0700

    Base

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - this the main function
- * description -prints all single digit number of base 10 starting from 0
+ * main - prints all single digit numbers of base 10 starting from 0
+ * followed by a new line.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
-Numberz
+base 10
