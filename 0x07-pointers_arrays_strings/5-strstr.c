e39922c1ea688a3e4fad07d1b5e510eb1dbf25e6
Festongithub <fkemboik@gmail.com>
2022-09-27 03:25:55 -0700

    init

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,18 +11,18 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	unsigned int i = 0, j =0;
-	
+	unsigned int i = 0, j = 0;
+
 	while (haystack[i])
 	{
 		while (needle[j] && (haystack[i] == needle[0]))
 		{
-			if (haystack [i + j} == needle[j])
+			if (haystack [i + j] == needle[j])
 				j++;
-			else 
-				break:
+			else
+				break;
 	}
-	if(needle[j])
+	if (needle[j])
 	{
 		i++;
 		j = 0;
