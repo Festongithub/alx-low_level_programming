40a202acb9f7b061e56e3ed2c64f088886da84b1
Festongithub <fkemboik@gmail.com>
2023-11-17 02:21:09 -0800

    name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - checks the code
+ * @argc:number of arguments
+ * @argv:array of arguments
+ *
+ * Return: always 0 on sucess
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
