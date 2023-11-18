794f688eeceb0c974a1fdb8520b373e72e0af9cd
Festongithub <fkemboik@gmail.com>
2023-11-18 01:58:43 -0800

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,10 +9,16 @@
  */
 void print_rev(char *s)
 {
-	if (*s == '\0')
+	int len = strlen(s);
+
+	if (s == NULL)
 	{
 		return;
 	}
-	print_rev(s + 1);
-	putchar(*s);
+
+	for (int i = len - 1; i >= 0; i--)
+	{
+		printf("%c", s[i]);
+	}
+	printf("\n");
 }
