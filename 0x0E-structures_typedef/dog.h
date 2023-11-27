402414cd1c57e006da10b13b8db2db1fe81003ed
Festongithub <fkemboik@gmail.com>
2023-11-27 05:38:52 -0800

    My dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _dog_h
+#define _dog_h
+/**
+ * struct dog-data type for dog
+ * @name: name of the dog
+ * @age:integer
+ * @owner: who owns the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
