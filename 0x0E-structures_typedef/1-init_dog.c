2092f6b51a61103b08149a722458209368661caf
Festongithub <fkemboik@gmail.com>
2023-11-28 08:10:40 -0800

    Dog is Power

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,16 +1,17 @@
 #include "dog.h"
-/**
-* init_dog - inits a variable of
-* type dog
-* @d: dog identification
-* @name: name of dog
-* @age: age of dog
-* @owner: owner's name
-*/
 
+/**
+ * init_dog - initializes a variable of type struct dog.
+ * @d: struct dog.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ *
+ * Return: no return.
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d )
+	if (d)
 	{
 		d->name = name;
 		d->age = age;
