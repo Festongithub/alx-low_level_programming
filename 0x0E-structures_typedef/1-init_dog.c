a4146f22abc5158e30963f846547e787ad797a98
Festongithub <fkemboik@gmail.com>
2023-11-28 07:12:19 -0800

    Dog is not behaving

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,8 +1,5 @@
-#include "dog.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <stddef.h>
-
+#include "dog.h"
 /**
 * init_dog - inits a variable of
 * type dog
@@ -16,8 +13,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-		(*d).name = name;
-		(*d).age = age;
-		(*d).owner = owner;
+		d.name = name;
+		d.age = age;
+		d.owner = owner;
 	}
 }
