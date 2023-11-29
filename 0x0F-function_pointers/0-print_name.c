aa1895312287b4048562d4fe9473935aedce8ef7
Festongithub <fkemboik@gmail.com>
2023-11-29 00:18:55 -0800

    Done with the day

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * print_name - prints name of  string
+ * @name: character to print
+ * @f:pointer to function
+ * Return: Always 0
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
