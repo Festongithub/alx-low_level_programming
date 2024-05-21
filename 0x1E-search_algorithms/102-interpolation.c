5dcdfa51db743d261de34d0c31b145fbf82eef6c
Festongithub <fkemboik@gmail.com>
2024-05-21 03:14:18 -0700

    Searcg algorithms

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * interpolation_search - searches for a value 
+ * interpolation_search - searches for a value
  * in a sorted array of integers using the Interpolation search
  * @array: pointer to the first element of the array to search in
  * @size: the number of elements in array
@@ -18,7 +18,8 @@ int interpolation_search(int *array, size_t size, int value)
 
 	for (low = 0, high = size - 1; high >= low;)
 	{
-		pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+		pos = low + (((double)(high - low) /
+		       (array[high] - array[low])) * (value - array[low]));
 
 		if (pos < size)
 			printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
